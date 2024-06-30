73e86b04fd356df4ffa37dbecce060b527769b1e
Adam Wuss <adam.wuss@extern.ferchau.com>
2024-06-30 23:26:47 +0200

    feat: add while

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ import os
 def download_mp3(url):
     yt = YouTube(url)
     audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
-    output_file = audio_stream.download()
+    download_folder = 'downloads'
+
+    if not os.path.exists(download_folder):
+        os.makedirs(download_folder)
+
+    output_file = audio_stream.download(output_path=download_folder)
     base, ext = os.path.splitext(output_file)
     new_file = base + '.mp3'
     os.rename(output_file, new_file)
     print(f'Pobrano: {new_file}')
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Pobierz audio z YouTube jako MP3')
-    parser.add_argument('url', type=str, help='URL filmu z YouTube')
-    args = parser.parse_args()
-    download_mp3(args.url)
+while True:
+    url = input("Provide YouTube link: ")
+    download_mp3(url)
